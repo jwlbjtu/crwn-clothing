c26f21db0fe11206e4bebdef529094828920c2da
jwlbjtu <jwlbjtu@gmail.com>
2019-11-09 12:39:53 -0500

    Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
+import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Hats from './pages/hats.component';
@@ -9,8 +10,11 @@ import Hats from './pages/hats.component';
 const App: React.FC = () => {
   return (
     <div>
-      <Route exact path='/' component={HomePage} />
-      <Route exact path='/shop' component={ShopPage} />
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
