cbf8ba6ab414c1ccd413710b59b429abb671e96a
Wenlong Jiang <jwlbjtu@gmail.com>
2020-01-22 22:59:49 -0500

    Add service-worker.js register, make the app PWA

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -8,6 +8,7 @@ import './index.css';
 import App from './App';
 
 import { store, persistor } from './redux/store';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <Provider store={ store }>
@@ -19,3 +20,5 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+
+serviceWorker.register();
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 const compression = require('compression');
+const enforce = require('express-sslify');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
@@ -18,6 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
+    app.use(enforce.HTTPS({ trustProtoHeader: true }));
     app.use(express.static(path.join(__dirname, 'client/build')));
 
     app.get('*', function(req, res) {
@@ -28,6 +30,10 @@ if (process.env.NODE_ENV === 'production') {
 app.listen(port, error => {
     if (error) throw error;
     console.log('Server running on port' + port);
+});
+
+app.get('./service-worker.js', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
 });
 
 app.post('/payment', (req, res) => {
